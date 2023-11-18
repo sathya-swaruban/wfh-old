985d15fd8578278618f9483cd8dc2de888f6955e
Sathya Swaruban <sathya.swaruban@outlook.com>
2023-11-18 21:41:33 +0530

    Update Changes

--- src/main/java/com/example/wfh/model/WfhRequest.java
+++ src/main/java/com/example/wfh/model/WfhRequest.java
@@ -1,11 +1,11 @@
 package com.example.wfh.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
 @Table("WFH_REQUEST")
@@ -20,10 +20,10 @@ public class WfhRequest {
     @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private LocalDate toDate;
 
-    @NotEmpty
-    private String fromTime;
+    @JsonFormat(pattern = "HH:mm", shape = JsonFormat.Shape.STRING)
+    private LocalTime fromTime;
 
-    @NotEmpty
-    private String toTime;
+    @JsonFormat(pattern = "HH:mm", shape = JsonFormat.Shape.STRING)
+    private LocalTime toTime;
 
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 create table if not exists wfh_request (
     id bigint auto_increment primary key,
-    from_date varchar(255),
-    to_date varchar(255),
-    from_time varchar(255),
-    to_time varchar(255)
+    from_date date,
+    to_date date,
+    from_time time,
+    to_time time
 );
